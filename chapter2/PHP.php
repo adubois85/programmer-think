012c0c4c548142860fb037e0967c31da573968a5
Alex DuBois <adubois@alumni.uci.edu>
2017-10-10 18:07:33 -0600

    function to create line of characters; whitespace elsewhere

--- chapter2/PHP.php
+++ chapter2/PHP.php
@@ -2,21 +2,29 @@
 function halfSquare() {
 	for($i = 5; $i >= 1; $i--) {
 		for ($j = $i; $j >= 1; $j--){
-			echo'#';
+			echo '#';
 		}
 		echo"\r\n";
 	}
-}
+};
 
 halfSquare();
 
 function fullSquare() {
 	for($i = 1; $i <= 5; $i++) {
 		for($j = 1; $j <= 5; $j++){
-			echo'#';
+			echo '#';
 		}
 		echo"\r\n";
 	}
-}
+};
 
 fullSquare();
+
+function line(){
+	for($i = 1; $i <=5; $i++) {
+		echo '#';
+	}
+};
+
+line();
