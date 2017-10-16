5afb63ee05bb294120070aaf4dbfb635d0f90adb
Alex DuBois <adubois@alumni.uci.edu>
2017-10-16 16:08:22 -0600

    Added regex for stripping non-number characters from a string

--- chapter2/input_processing.php
+++ chapter2/input_processing.php
@@ -6,11 +6,20 @@
  // digit doubling function
 
  function doubleDigit(int $digit) {
-     $double = $digit * 2;
-     if ($double >= 10) {
-         $sum = $double - 9;
-         return $sum;
-     } else {
-         return $double;
-     }
+	 $double = $digit * 2;
+	 if ($double >= 10) {
+		 $sum = $double - 9;
+		 return $sum;
+	 } else {
+		 return $double;
+	 }
  }
+
+ // use a regular expression to strip any non-number characters from our input
+
+ function removeNonNumbers($input) {
+	$result = preg_replace("/\D/", "", $input);
+	return $result;
+ }
+$test = "Hello World 19";
+ removeNonNumbers($test);
