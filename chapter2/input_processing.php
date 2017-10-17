e8e5878ef7d971b02706e4ac602501e13f8f82f8
Alex DuBois <adubois@alumni.uci.edu>
2017-10-16 19:17:10 -0600

    Finished function for Luhn checksum

--- chapter2/input_processing.php
+++ chapter2/input_processing.php
@@ -3,32 +3,57 @@
  * Functions to implement a Luhn Checksum on a number of arbitrary length
  */
 
- // digit doubling function
+// digit doubling function
 
- function doubleDigit(int $digit) {
-	 $double = $digit * 2;
-	 if ($double >= 10) {
-		 $sum = $double - 9;
-		 return $sum;
-	 } else {
-		 return $double;
-	 }
- }
+function doubleDigit(int $digit) {
+	$double = $digit * 2;
+	if ($double >= 10) {
+		$sum = $double - 9;
+		return $sum;
+	} else {
+		return $double;
+	}
+}
 
- // use a regular expression to strip any non-number characters from our input
+// use a regular expression to strip any non-number characters from our input
 
- function removeNonNumbers($input) {
+function removeNonNumbers($input) {
 	$result = preg_replace("/\D/", "", $input);
 	return $result;
- }
+}
 
- // We'll cast our input to an array and use that to do our math
- // We'll also start filling from right to left for ease later on
+// we'll cast our input to an array and use that to do our math
+// we'll also start filling from right to left for ease later on
 
- function fillArray($input){
-	 $array = array();
-	 for($i = strlen($input) - 1; $i >= 0; $i--){
-		 $array[] = $input[$i];
-	 }
-	 return $array;
- }
+function fillArray($input){
+	$array = array();
+	for($i = strlen($input) - 1; $i >= 0; $i--){
+		$array[] = $input[$i];
+	}
+	return $array;
+}
+
+// bringing it all together, the math part of the algorithm where we add all of the digits
+
+function luhnCheck($number) {
+	// strip the non-numbers from our input
+	$number = removeNonNumbers($number);
+	//cast them to an array in reverse order
+	$array = fillArray($number);
+	// the value where we'll store the sum of the digits
+	$sum = 0;
+	foreach($array as $key => $value) {
+		if(($key + 1) % 2 === 0){
+			$sum += doubleDigit($value);
+		} 
+		else {
+			$sum += $value
+		}
+	}
+	if($sum % 10 === 0){
+		echo"The entered number is valid.\n";
+	}
+	else{
+		echo"The number entered is not valid.\n";
+	}
+}
