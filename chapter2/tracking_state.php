e79c1aa0ce9fa4c639618cc1e4148ee714904875
Alex DuBois <adubois@alumni.uci.edu>
2017-10-31 17:18:23 -0600

    Initial set up / regex for tracking state problem

--- chapter2/tracking_state.php
+++ chapter2/tracking_state.php
@@ -1,1 +1,18 @@
 <?php
+
+/**
+ * Tracker for the current decryption mode
+ * 0 = Uppercase
+ * 1 = Lowercase
+ * 2 = punctuation
+ * 
+ * Modes should always move from 0->1->2->0->etc.
+ */
+
+$decryptMode = 0;
+
+// preparing the regular expression to filter and set up our numbers
+$pattern  = '/\d+/';
+$subject ='';
+
+preg_match_all($pattern, $subject, $matches);
