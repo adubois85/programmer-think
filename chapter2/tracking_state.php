90bc180ca1e23019802df2947d26d3a6bd3f6554
Alex DuBois <adubois@alumni.uci.edu>
2017-10-31 17:26:15 -0600

    moved tracking state setup code into a function for ease later

--- chapter2/tracking_state.php
+++ chapter2/tracking_state.php
@@ -1,18 +1,19 @@
 <?php
 
-/**
- * Tracker for the current decryption mode
- * 0 = Uppercase
- * 1 = Lowercase
- * 2 = punctuation
- * 
- * Modes should always move from 0->1->2->0->etc.
- */
+function trackingState(){/**
+    * Tracker for the current decryption mode
+    * 0 = Uppercase
+    * 1 = Lowercase
+    * 2 = punctuation
+    * 
+    * Modes should always move from 0->1->2->0->etc.
+    */
 
-$decryptMode = 0;
+    $decryptMode = 0;
+   
+    // preparing the regular expression to filter and set up our numbers
+    $pattern  = '/\d+/';
+    $subject ='';
+    preg_match_all($pattern, $subject, $matches);
 
-// preparing the regular expression to filter and set up our numbers
-$pattern  = '/\d+/';
-$subject ='';
-
-preg_match_all($pattern, $subject, $matches);
+}
