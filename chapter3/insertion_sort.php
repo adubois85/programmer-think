d6b743e087d0f02372cab081346e5081c5bae9c5
Alex DuBois <adubois@alumni.uci.edu>
2017-11-07 16:09:57 -0700

    Initial work on an insertion sort algorithm

---
+++ chapter3/insertion_sort.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * An algorithm for sorting using an insertion method
+ */
+
+ function sortInsertion(array $inputArray){
+	// note: create a variable that stores the size of the array so you aren't counting it with every iteration of a for loop; otherwise, a foreach loop will also work and is very slightly faster
+	$arraySize = count($inputArray);
+	// outer loop to go through each item in the array
+	for ($i = 1; $i < $arraySize; $i++){
+		for ($j = $i; $j > )
+	}
+ }
